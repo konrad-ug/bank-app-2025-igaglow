55ef05b5f577193c6c6a1fd76105a8fbf984e531
Iga Głowacz <iglowacz@INF.UG.EDU.PL>
2025-10-16 13:34:19 +0200

    Walidacja peselu

--- src/account.py
+++ src/account.py
@@ -1,5 +1,12 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0.0
+        self.pesel = self.validate_pesel(pesel)
+
+    def validate_pesel(self, pesel):
+        if(len(pesel) == 11):
+            return pesel
+        else:
+            return "Invalid"
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,16 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "05242206607")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
+        assert account.pesel == "05242206607"
+
+    def test_pesel_too_short(self):
+        account = Account("Jane", "Doe", "111")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_too_long(self):
+        account = Account("Jane", "Doe", "1111111111111111111")
+        assert account.pesel == "Invalid"
